5ced21a49a329f01301b605a18a4c7aea27d69d5
n.kanreky <n_kanreky_ta@yahoo.co.jp>
2016-11-10 22:14:37 +0900

    SecondKadaiApp resubmission

--- SecondKadaiApp/ResultViewController.swift
+++ SecondKadaiApp/ResultViewController.swift
@@ -12,34 +12,30 @@ import UIKit
 class ResultViewController: UIViewController {
     
     
+    
+    //2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
     @IBOutlet weak var label: UILabel!
-    // 2画面目のLabelを、StoryboardでこのViewControllerにIBOutletとして接続しておく
     
     
     // 受け取るためのプロパティ（変数）を宣言しておく
-    
+    //上書きが前提なのでvarが必要　　？をつける
     var inputName:String?
     
-    //上書きが前提なのでvarが必要　　？をつける
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
+       
+    // Do any additional setup after loading the view.
         
         label.text = "こんにちは \(inputName!) さん"
     }
     
-    //¥は＼　option¥
-    //「こんにちは　〜　さん」の〜に、第一画面で入力した名前を表示　\をつけないと　“　”がついてしまう 
-    // \はoption ¥で変換　英数で入れる
-    // \を間違えると、それから（inputName!)までがstringとして表示される
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
     }
 }
-//
+
 
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -16,26 +16,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var textName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
-        // segueから遷移先のResultViewControllerを取得する
+    // segueから遷移先のResultViewControllerを取得する
+    let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
         
-        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
-        // 遷移先のResultViewControllerで宣言しているstringに値を代入して渡す
+    // 遷移先のResultViewControllerで宣言しているstringに値を代入して渡す
         
         resultViewController.inputName = textName.text
         
     }
-    //上では、textに名前を入力させるので、textNameと命名
     
     @IBAction func unwind(segue: UIStoryboardSegue) {
-        // 他の画面から segue を使って戻ってくる時に呼ばれる
+    // 他の画面から segue を使って戻ってくる時に呼ばれる
     }
 }
 
